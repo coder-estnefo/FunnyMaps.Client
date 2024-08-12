7f913d8b1b56ea00e176bd7521b05e2b0d45b6b1
Ofentse Masita <ofentsemasita@gmail.com>
2024-08-12 19:40:54 +0200

    Add Search Jokes By Location functionality

--- src/app/pages/funny-map/funny-map.component.ts
+++ src/app/pages/funny-map/funny-map.component.ts
@@ -1,8 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 import * as turf from '@turf/turf';
 import { environment } from 'src/environments/environment.development';
+import { IGeocoding } from 'src/app/interfaces/igeocoding';
+import { JokeService } from 'src/app/services/joke/joke.service';
+import { Joke } from 'src/app/interfaces/joke';
 
 @Component({
   selector: 'app-funny-map',
@@ -12,7 +15,14 @@ import { environment } from 'src/environments/environment.development';
 export class FunnyMapComponent implements OnInit, AfterViewInit {
   private token = environment.mapbox_access_token;
 
+  jokeService = inject(JokeService);
+
   map?: mapboxgl.Map;
+  locationQuery!: string;
+  currentLocation!: string;
+  places?: IGeocoding;
+  jokes: Joke[] = [];
+  isLoading = false;
 
   constructor() {}
 
@@ -33,6 +43,27 @@ export class FunnyMapComponent implements OnInit, AfterViewInit {
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [28.14094120767747, -25.75542520300563],
       zoom: 4,
+    });
+  }
+
+  searchForLocation() {
+    if (this.locationQuery?.trim() != '') {
+      this.jokeService.getLocations(this.locationQuery).subscribe((res) => {
+        this.places = res;
+      });
+    } else {
+      this.places = undefined;
+    }
+  }
+
+  selectLocation(place: string, lng: number, lat: number) {
+    this.isLoading = true;
+    this.places = undefined;
+    this.locationQuery = place;
+    this.currentLocation = place;
+    this.jokeService.getJokesByLocation(place).subscribe((jokes) => {
+      this.isLoading = false;
+      this.jokes = jokes;
     });
   }
 }
--- src/app/pages/funny-map/funny-map.module.ts
+++ src/app/pages/funny-map/funny-map.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { FunnyMapRoutingModule } from './funny-map-routing.module';
 import { FunnyMapComponent } from './funny-map.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatModules } from 'src/app/app.module';
 
 @NgModule({
   declarations: [FunnyMapComponent],
-  imports: [CommonModule, FunnyMapRoutingModule, MatModules],
+  imports: [
+    CommonModule,
+    FunnyMapRoutingModule,
+    MatModules,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class FunnyMapModule {}
